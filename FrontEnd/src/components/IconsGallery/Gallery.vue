e486038e00c2258c6b4e2ea4cdc24803a8e0cf58
muhammad <muhammad.yahya@microfocus.com>
2020-11-20 18:11:40 +0200

    updated gallery to import all icons from SvgSprite

--- FrontEnd/src/components/IconsGallery/Gallery.vue
+++ FrontEnd/src/components/IconsGallery/Gallery.vue
@@ -1,19 +1,34 @@
 <template>
     <div>
+        <SvgSprite ref="svgSprite"></SvgSprite>
         <IconList :iconsList="iconsList"></IconList>
     </div>
 </template>
 
 <script>
     import IconList from "@/components/IconsGallery/IconsList.vue";
-    import iconsList from "./iconsList";
+    import SvgSprite from "@/components/Shared/SvgSprite.vue";
     export default {
         name: "Gallery",
-        components: {IconList},
+        components: {IconList, SvgSprite},
         data() {
             return {
-                iconsList
+                iconsList: []
             };
+        },
+        mounted() {
+            /* Extract all icons id with from all of the elements <symbol> */
+            const symbolRegex = new RegExp(`(?<=symbol).*?(?=>)`, "g");
+            const idRegex = new RegExp(`(?<=id=").*?(?=")`);
+            const svgSpriteTemplate = this.$refs["svgSprite"].$el.outerHTML;
+            this.iconsList = svgSpriteTemplate
+                .match(symbolRegex)
+                .filter(symbol => symbol !== "")
+                .map(symbol => {
+                    if (symbol !== "") {
+                        return {name: symbol.match(idRegex)[0]};
+                    }
+                });
         }
     };
 </script>
