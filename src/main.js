6d3ae0a8c7fd7be69890f943a4315d178d352112
yahyamuh <muhammad.yahya@microfocus.com>
2021-06-20 16:56:27 +0300

    added firebase the project

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import store from "./store";
 import VueRouter from "vue-router";
 import routes from "@/routes";
 
+/* create firebase.config using the information in firebase */
+import firebaseConfig from "../firebase.config";
+import firebase from "firebase/app";
+
+firebase.initializeApp(firebaseConfig);
+
 const router = new VueRouter({
     mode: "history",
     base: process.env.BASE_URL,
